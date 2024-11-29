2a33a23b3f66024551878c524ac55e479db6b7f0
celsius narhwal <hello@celsiusnarhwal.dev>
2024-11-29 17:14:13 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,12 +3,15 @@ import typing as t
 from fastapi import Depends, FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pathlib import Path
 
 app = FastAPI()
 security = HTTPBasic()
 
 
-@app.get("/simple/fontawesomepro/{path:path}")
-async def proxy(path: Path, credentials: t.Annotated[HTTPBasicCredentials, Depends(security)]):
-    return RedirectResponse(f"https://dl.fontawesome.com/{credentials.password}/fontawesome-pro/python/simple/{path}")
+@app.get("/{path:path}")
+async def proxy(
+    path: str, credentials: t.Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    return RedirectResponse(
+        f"https://dl.fontawesome.com/{credentials.password}/fontawesome-pro/python/{path}"
+    )
