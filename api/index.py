5f3f9e7b426802379988f9885a6727083cb52fcb
celsius narhwal <hello@celsiusnarhwal.dev>
2024-11-29 16:04:42 -0500

    Initial commit

---
+++ api/index.py
@@ -0,0 +1,14 @@
+import typing as t
+
+from fastapi import Depends, FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+
+app = FastAPI()
+security = HTTPBasic()
+
+
+@app.get("/")
+async def proxy(credentials: t.Annotated[HTTPBasicCredentials, Depends(security)]):
+    return RedirectResponse(f"https://dl.fontawesome.com/{credentials.password}/fontawesome-pro/python/simple")
